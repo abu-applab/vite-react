80e0ccf72e9e459a7e34df7e9033d63c9588a975
Abubakkar <abubakkar@applab.qa>
2025-09-07 14:22:13 +0530

    Fix: Deployment issue

--- server.cjs
+++ server.cjs
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle SPA routing (if you're using React Router)
-app.get('*wildcard', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
