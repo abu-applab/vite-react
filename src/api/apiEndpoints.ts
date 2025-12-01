bab3ece51b4cef62e39eeda58e1e429e011eb01f
Aashique P A <aashique.p@applabs.qa>
2025-12-01 16:59:38 +0530

    fix: proxy remove and added on the base url

--- src/api/apiEndpoints.ts
+++ src/api/apiEndpoints.ts
@@ -1,24 +1,24 @@
 // /network/apiEndpoints.js
 
 export const API_ENDPOINTS = {
-  getPlots: '/Proxy/WebApp/getPlots',
-  getSignatories: '/Proxy/WebApp/getSignatories',
-  getCompanies: '/Proxy/WebApp/getCompanies',
-  getClusters: '/Proxy/WebApp/getClusters',
-  getLocations: '/Proxy/WebApp/getLocations',
-  getHSCodes: '/Proxy/WebApp/getHSCodes',
-  createApplication: '/Proxy/WebApp/createApplication',
-  updateApplication: '/Proxy/WebApp/updateApplication',
-  deleteApplication: '/Proxy/WebApp/deleteApplication',
-  submitApplication: '/Proxy/WebApp/submitApplication',
-  getISICSections: '/Proxy/WebApp/getISICSections',
-  getISICCodesBySectionId: '/Proxy/WebApp/getISICCodesBySectionId',
-  getAllServiceRequests: "/Proxy/WebApp/getAllServiceRequests",
-  getApplicationsList: '/Proxy/WebApp/getApplicationsList',
-  getApplication: '/Proxy/WebApp/getApplication',
-  getFindingsList: "/Proxy/WebApp/getFindingsListFromOP",
-  GetFindingFromOP: "/Proxy/WebApp/GetFindingFromOP",
-  updateFindingDetails: "/Proxy/WebApp/updateFindingDetails",
+  getPlots: '/WebApp/getPlots',
+  getSignatories: '/WebApp/getSignatories',
+  getCompanies: '/WebApp/getCompanies',
+  getClusters: '/WebApp/getClusters',
+  getLocations: '/WebApp/getLocations',
+  getHSCodes: '/WebApp/getHSCodes',
+  createApplication: '/WebApp/createApplication',
+  updateApplication: '/WebApp/updateApplication',
+  deleteApplication: '/WebApp/deleteApplication',
+  submitApplication: '/WebApp/submitApplication',
+  getISICSections: '/WebApp/getISICSections',
+  getISICCodesBySectionId: '/WebApp/getISICCodesBySectionId',
+  getAllServiceRequests: "/WebApp/getAllServiceRequests",
+  getApplicationsList: '/WebApp/getApplicationsList',
+  getApplication: '/WebApp/getApplication',
+  getFindingsList: "/WebApp/getFindingsListFromOP",
+  GetFindingFromOP: "/WebApp/GetFindingFromOP",
+  updateFindingDetails: "/WebApp/updateFindingDetails",
   logIn: "/RegistrationAndLogin/login",
   logOut: "/RegistrationAndLogin/logout",
   signUp: "/RegistrationAndLogin/signup",
@@ -26,54 +26,54 @@ export const API_ENDPOINTS = {
   resendOtp: "/RegistrationAndLogin/resend-otp",
 
   forgotpassword: "/RegistrationAndLogin/requestPasswordReset",
-  resetPassword: "/Proxy/RegistrationAndLogin/resetPassword",
+  resetPassword: "/RegistrationAndLogin/resetPassword",
 }
 
 export const API_SERVICES_ENDPOINTS = {
   rentalRelationship: {
-    url: "/Proxy/WebApp/createRentalRelationRequest",
+    url: "/WebApp/createRentalRelationRequest",
     method: "POST",
     contentType: "multipart",
   },
   landTransfer: {
-    url: "/Proxy/WebApp/createLandTransfer",
+    url: "/WebApp/createLandTransfer",
     method: "POST",
     contentType: "multipart",
   },
   certifiedCopyOfAgreement: {
-    url: "/Proxy/WebApp/createCertifiedCopyOfAgreementRequest",
+    url: "/WebApp/createCertifiedCopyOfAgreementRequest",
     method: "POST",
     contentType: "json",
   },
   demarcationLetter: {
-    url: "/Proxy/WebApp/createDemarcationLetterRequest",
+    url: "/WebApp/createDemarcationLetterRequest",
     method: "POST",
     contentType: "json",
   },
   complaint: {
-    url: "/Proxy/WebApp/createComplaintRequest",
+    url: "/WebApp/createComplaintRequest",
     method: "POST",
     contentType: "multipart",
   },
   technicalQueries: {
-    url: "/Proxy/WebApp/createTechnicalQueriesRequest",
+    url: "/WebApp/createTechnicalQueriesRequest",
     method: "POST",
     contentType: "json",
   },
   kahramaa: {
-    url: "/Proxy/WebApp/createKahramaRequest",
+    url: "/WebApp/createKahramaRequest",
     method: "POST",
     contentType: "json",
   },
   updateContactDetails: {
-    url: "/Proxy/WebApp/createUpdateContactDetailRequest",
+    url: "/WebApp/createUpdateContactDetailRequest",
     method: "POST",
     contentType: "multipart",
   },
   updateCompanyInformation: {
     urls: [
-      "/Proxy/WebApp/createBasicCompanyUpdateRequest",
-      "/Proxy/WebApp/updateCompanyUpdateRequestDetails"
+      "/WebApp/createBasicCompanyUpdateRequest",
+      "/WebApp/updateCompanyUpdateRequestDetails"
     ],
     method: "POST",
     contentType: "multipart",
